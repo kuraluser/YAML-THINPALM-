2eb5f5194cfd1ad93b9222d4e0e107b0ad6e7ffb
Tan Peng Hui <tanpeng.hui@alphaori.sg>
2021-04-15 20:11:09 +0800

    Minor Fix

--- loadableStudy/main.py
+++ loadableStudy/main.py
@@ -20,7 +20,7 @@ import json
 from api_vlcc import gen_allocation, loadicator
 
 import httpx
-
+from loguru import logger
 
 ## load configuration --------------------------------------------------------
 with open('config.json', "r") as f_:
@@ -47,6 +47,7 @@ engine = sqlalchemy.create_engine(DATABASE_URL)
 metadata.create_all(engine)
 
 app = FastAPI()
+logger.add(config['log']['fileName'], rotation=config['log']['rotation'], backtrace=True, diagnose=True)
 
 class DbList(BaseModel):
     id: str
@@ -58,7 +59,7 @@ class DbList(BaseModel):
 class DbDelete(BaseModel):
     id: str = Field(..., example="Enter the id")
 
-async def get_vessel_details(url):
+async def get_vessel_details(url, gID):
     try:
         async with httpx.AsyncClient() as client:
             resp_ = await client.get(url)
@@ -66,23 +67,27 @@ async def get_vessel_details(url):
             if resp_.status_code == httpx.codes.OK:
                 response_json = resp_.json()
                 
-    except Exception as err:
-        print(f"HTTP Exception: {err}")
-        print('>>>> get vessel FILE')
+    except httpx.HTTPError as err:
+        # print(f"HTTP Exception: {err}")
+        # print('>>>> get vessel FILE')
+        logger.info(gID + ": " + f"HTTP Exception - {err}")
+        logger.info(gID + ": Get vessel FILE")
         with open('vessel_info.json') as json_file: 
             response_json = json.load(json_file) 
             
     return response_json
 
 
-async def post_response(url, payload):
+async def post_response(url, payload, gID):
     try:
         async with httpx.AsyncClient() as client:
             resp_ = await client.post(url, json=payload)
             resp_.raise_for_status()
-            print(url,resp_)
-    except Exception  as err:
-        print(f"HTTP Exception: {err}")
+            # print(url,resp_)
+            logger.info(gID + ": "+ resp_)
+    except httpx.HTTPError  as err:
+        # print(f"HTTP Exception: {err}")
+        logger.info(gID + ": "+ f"HTTP Exception - {err}")
         pass
 
 async def run_in_process(fn, *args):
@@ -103,16 +108,19 @@ async def start_cpu_bound_task(uid: str, data: dict) -> None:
             )
     await database.execute(query)
     
+    logger.info(uid + ": Allocation completed")
     ## send feedback
-    print('>>>> Update feedback to Thinkpalm')
-    status_url_ = config['url']['loadable-study-status'].format(vesselId=data['loadable']['vesselId'],
-                                                                voyageId=data['loadable']['voyageId'],
-                                                                loadableStudyId=data['loadable']['id'])
-    # print(status_url_)
-    await post_response(status_url_, {"processId" : uid, "loadableStudyStatusId" : 5})
+    # print('>>>> Update feedback to Thinkpalm')
     
     
     if result.get('validated', None) in [None]:
+        logger.info(uid + ": Update status")
+        status_url_ = config['url']['loadable-study-status'].format(vesselId=data['loadable']['vesselId'],
+                                                                voyageId=data['loadable']['voyageId'],
+                                                                loadableStudyId=data['loadable']['id'])
+        # print(status_url_)
+        await post_response(status_url_, {"processId" : uid, "loadableStudyStatusId" : 5}, uid)
+    
         result_url_ = config['url']['loadable-patterns'].format(vesselId=data['loadable']['vesselId'],
                                                                 voyageId=data['loadable']['voyageId'],
                                                                 loadableStudyId=data['loadable']['id'])
@@ -122,7 +130,8 @@ async def start_cpu_bound_task(uid: str, data: dict) -> None:
                                                                 loadableStudyId=data['loadable']['id'],
                                                                 loadablePatternId=data['loadable']['loadablePatternId'])
     print(result_url_)
-    await post_response(result_url_, result)
+    logger.info(uid + ": Upload result")
+    await post_response(result_url_, result, uid)
     
 
 @app.post("/new_loadable", status_code=HTTPStatus.ACCEPTED)
@@ -156,12 +165,14 @@ async def task_handler(data: dict, background_tasks: BackgroundTasks):
     data_['processId'] = gID
     # data_['ballastEdited'] = True
     
-    print('>>>> get vessel API')
+    # print('>>>> get vessel API')
+    logger.info(gID + ": Get vessel API")
     vessel_url_ = config['url']['vessel-details'].format(vesselId=data_['loadable']['vesselId'])
     # print(vessel_url_)
-    data_['vessel'] = await get_vessel_details(vessel_url_)
+    data_['vessel'] = await get_vessel_details(vessel_url_, gID)
         
-    print('>>>> add new loadable')
+    # print('>>>> add new loadable')
+    logger.info(gID + ": Add new loadable")
     background_tasks.add_task(start_cpu_bound_task, gID, data_)
 
     ## send feedback
@@ -191,7 +202,8 @@ async def loadicator_handler(data: dict):
     result = await database.fetch_val(query,column=2)
     limits = result['message']['limits']
     out = loadicator(data, limits)
-    print('>>>Send loadicator results')
+    # print('>>>Send loadicator results')
+    logger.info(data["processId"] + ": Upload loadicator result")
     loadicator_url_ = config['url']['loadable-patterns'].format(vesselId=limits['vesselId'],voyageId=limits['voyageId'],loadableStudyId=limits['id'])
     # print(status_url_)
     await post_response(loadicator_url_, out)
