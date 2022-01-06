79f0f6e9cd4f84ca60cd89b891fd027b93a6a17a
Tan Peng Hui <tanpeng.hui@alphaori.sg>
2022-01-06 18:09:44 +0800

    Update main.py

--- loadableStudy/main.py
+++ loadableStudy/main.py
@@ -264,8 +264,7 @@ async def start_cpu_bound_task(uid: str, data: dict) -> None:
         r_ = r_.replace("\n", " ")
         r_ = r_.replace('"','')
         
-        result = {'processId': uid,
-                  "errors": [{'processId': uid},{"errorHeading": "ALGO Internal Server Error", "errorDetails": [r_]}]}
+        result = {"errors": [{"errorHeading": "ALGO Internal Server Error", "errorDetails": [r_, uid]}]}
         result = json.loads(json.dumps(result))
 
         #print("the res ",result)
