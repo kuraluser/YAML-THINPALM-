1c8c84ef91c565be52165b2bbd32f85fa5cb3e3e
Tan Peng Hui <tanpeng.hui@alphaori.sg>
2022-01-28 13:46:15 +0800

    Return entire loading info in loading module

--- loadableStudy/loading_init.py
+++ loadableStudy/loading_init.py
@@ -56,11 +56,7 @@ class Process_input(object):
                                   "loadingMachinesInUses":data['loading']['loadingInformation']["machineryInUses"]["loadingMachinesInUses"]
                                   }
         
-        self.loading_information = {"loadingRates":data['loading']['loadingInformation']["loadingRates"],
-                                    "berthDetails":data['loading']['loadingInformation']['berthDetails'],
-                                    "loadingSequences":data['loading']['loadingInformation']["loadingSequences"],
-                                    "machineryInUses":data['loading']['loadingInformation']["machineryInUses"]
-                                    }
+        self.loading_information = data['loading']['loadingInformation']
         
         
         self.config = data['config']
